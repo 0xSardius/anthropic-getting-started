0f0a1ba47ac5a43499148da3cc6019643ffcf2be
Sardius <0xsardius@gmail.com>
2025-03-16 08:19:07 -0400

    params update

--- src/examples/parameters.py
+++ src/examples/parameters.py
@@ -5,3 +5,23 @@ load_dotenv()
 
 client = Anthropic()
 
+truncated_response = client.messages.create(
+    model="claude-3-haiku-20240307",
+    max_tokens=10,
+    messages=[
+        {"role": "user", "content": "Write me a poem"}
+    ]
+)
+print(truncated_response.content[0].text)
+print(truncated_response.stop_reason)
+
+
+longer_poem_response = client.messages.create(
+    model="claude-3-haiku-20240307",
+    max_tokens=500,
+    messages=[
+        {"role": "user", "content": "Write me a poem about lemongrab from the show Adventure Time"}
+    ]
+)
+print(longer_poem_response.content[0].text)
+print(longer_poem_response.stop_reason)
